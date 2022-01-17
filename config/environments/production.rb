75f96b22d2c62c3ef1d9065c087b4ba4cd72a7d0
Takudai Kawasaki <ouch.taguhoiya@outlook.jp>
2022-01-17 17:09:28 +0900

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,18 +63,18 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_options = { from: Rails.application.credentials.mailer[:email] }
-  config.action_mailer.default_url_options = { host: 'http://54.65.115.152/' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    user_name: Rails.application.credentials.mailer[:email],
-    password: Rails.application.credentials.mailer[:password],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.default_options = { from: Rails.application.credentials.mailer[:email] }
+  # config.action_mailer.default_url_options = { host: 'http://54.65.115.152/' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: 'gmail.com',
+  #   user_name: Rails.application.credentials.mailer[:email],
+  #   password: Rails.application.credentials.mailer[:password],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
