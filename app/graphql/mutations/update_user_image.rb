b313ec54a22737292ba2f5451779abddef8d0283
Takudai Kawasaki <ouch.taguhoiya@outlook.jp>
2022-01-22 23:22:35 +0900

    add nickname argument in update-user-image

--- app/graphql/mutations/update_user_image.rb
+++ app/graphql/mutations/update_user_image.rb
@@ -5,12 +5,22 @@ module Mutations
     field :user, Types::UserType, null: true
 
     argument :id, ID, required: true
+    argument :nickname, String, required: false
     argument :image, ApolloUploadServer::Upload, required: false
     def resolve(**args)
       user = User.find(args[:id])
-      image = args[:image]
-      user.file.attach(io: image.to_io, filename: image.original_filename)
-      user.update!(image: args[:image])
+      if args[:image] && args[:nickname]
+        image = args[:image]
+        user.file.attach(io: image.to_io, filename: image.original_filename)
+        user.update!(image: args[:image], nickname: args[:nickname])
+      elsif args[:image].nil?
+        user.update!(nickname: args[:nickname])
+      elsif args[:nickname].nil?
+        image = args[:image]
+        user.file.attach(io: image.to_io, filename: image.original_filename)
+        user.update!(image: args[:image], nickname: args[:nickname])
+        user.update!(image: args[:image])
+      end
       {
         user: user
       }
