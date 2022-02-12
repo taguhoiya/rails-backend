187c07f50a4da2d279afc9e6f0672380a6932130
Takudai Kawasaki <ouch.taguhoiya@outlook.jp>
2022-02-12 12:32:26 +0900

    add http on cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'localhost:3001', 'https://moviewapp.com'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'https://moviewapp.com'
 
     resource '*',
              headers: :any,
